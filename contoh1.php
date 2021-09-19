d149135cff1911a9930cc64c13baafc7ba29cfab
ilhamutomo1 <ilhamtriputroutomo@gmail.com>
2021-09-19 14:28:03 +0700

    menambahkan isi contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Ilham Triputro Utomo
+        Saya tinggal di daerah Jakarta
+        Olahraga yang saya sukai adalah Skateboard";
+    }
+} ?>
