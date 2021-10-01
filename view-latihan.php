0133ad92b1ae5a80985c6f0484fed3733271be5e
ilhamutomo1 <ilhamtriputroutomo@gmail.com>
2021-10-01 12:03:09 +0700

    menambahkan isi file view-latihan

---
+++ view-latihan.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        Hallo Sobat.. Yuk kita belajar web programming!<br>
+        Nilai 1 = <?= $nilai1; ?>
+        Nilai 2 = <?= $nilai2; ?>
+        ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . $hasil; ?>
+    </body>
+</html>
